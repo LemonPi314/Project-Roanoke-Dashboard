f96f2adf538063599c33bf8f2a65328607af7f25
Timothy Pavlushik <timothy.pavlushik@gmail.com>
2021-02-11 19:18:05 -0800

    Massive update.

--- Project-Roanoke/Project_Roanoke.py
+++ Project-Roanoke/Project_Roanoke.py
@@ -1,112 +1,127 @@
-import webbrowser
-import os
-import requests
+# Imports needed to use this code
+import os, requests, socket, webbrowser
 from requests import get
-import socket
+from PythonTools import *
 
-#Fixes weird bug in windows command line to allow printing of colors. 
-os.system('cls')
+# Attempt at making the PythonTools library be downloaded from GitHub
+#from httpimport import github_repo
+#with github_repo("Duplexes", "PythonTools"):
+#    from PythonTools import PythonTools
 
+titleDir = os.path.join(projectDir, "titles")
 
- #Functions 
-#Checks thats the users input is a valid number between 1 - 10
-def userinput():
-    user_input = input("Please select a program 1- 10: ")
-   
+# Functions
+# Main startup screen
+def MainDashboard():
+    ClearScreen()
+    titles = ListFiles(titleDir, "title-project-roanoke")
+    titleColors = [BrightCyan, BrightYellow, BrightGreen, BrightYellow, BrightGreen]
+    titleNumber = 0
+    for title in titles:
+        content = open(title)
+        PrintMessage(content.read(), "none", titleColors[titleNumber])
+        titleNumber += 1
 
+    UserInput()
+
+# Clears the console screen
+def ClearScreen():
+    if os.name == "posix":
+        os.system("clear")
+    else:
+       os.system("cls")
+
+# Searches for files in a directory matching the filter
+def ListFiles(filepath, filetype):
+   paths = []
+   for root, dirs, files in os.walk(filepath):
+      for file in files:
+         if file.lower().startswith(filetype.lower()):
+            paths.append(os.path.join(root, file))
+   return(paths)
+
+# Checks that the users input is a valid number between 1-18
+def UserInput():
+    user_input = input("Please select a program 1-18: ")
     try:
-        val = int(user_input)
-        if val > 10:
-            print("\u001b[31mNumber is greater then 10.\u001b[0m")
-            userinput()
-        elif val < 0:  
-            print("\u001b[31mWhy would you think to use negative numbers? How would that even work.\u001b[0m")
-        elif val == 0:
-            print("\u001b[31mPlease use a number greator then 1.\u001b[0m")
-            userinput()
+        value = int(user_input)
+        if value > 18:
+            PrintMessage("Number is greater than 18.", "error")
+            UserInput()
+        elif value < 0:
+            PrintMessage("Why would you think to use negative numbers? How would that even work?", "error")
+            UserInput()
+        elif value == 0:
+            PrintMessage("Please enter a number greater than 0.", "error")
+            UserInput()
 
-        #Start of selection elif statments. This 
-
-        #Selection: 1 - Opens web rickroll - TEMP 
-        elif val == 1:
-            print("\u001b[32mBooting selected program this may take a second...\u001b[0m")
-            webbrowser.register('chrome',
+        # Start of selection elif statments
+        # Selection: 1 - Opens web rickroll - TEMPORARY
+        elif value == 1:
+            PrintMessage("Booting selected program. This may take a second...")
+            webbrowser.register("chrome",
 	            None,
 	            webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
-            webbrowser.get('chrome').open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-        #Selection: 2 - Opens Fern Wifi Cracker - Placeholder 
-        elif val == 2:
-            print("Fern Wifi Cracker PlaceHolder")
-        #Selection: 10 - Opens the IP info applet 
-        elif val == 10: 
-            ipinfo()
-    #If a user enters somthing there not supposed to this runs. 
+            webbrowser.get("chrome").open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+        # Selection: 2 - Opens Fern Wifi Cracker - Placeholder 
+        elif value == 2:
+            ClearScreen()
+            PrintMessage("Fern Wifi Cracker Placeholder")
+            input("Press ENTER to continue.")
+
+        # Selection: 10 - Opens the IP Info applet 
+        elif value == 10:
+            IPInfo()
+
+    # If the user enters something they"re not supposed to
     except ValueError:
         try:
-            val = float(user_input)
-            print("\u001b[31mYour really trying to break my program please stop.\u001b[0m")
-            userinput()
+            value = float(user_input)
+            PrintMessage("You're really trying to break my program. Please stop.", "error")
+            UserInput()
+
         except ValueError:
-            print("\u001b[31mPlease enter a number, not a string.\u001b[0m")
-            userinput()
+            PrintMessage("Please enter a number, not a string.", "error")
+            UserInput()
 
-#Gets information about public ip adress and private IPv4 adress. 
-def ipinfo():
-    os.system('cls')
-    publicip = get('https://api.ipify.org').text
-    hostname = socket.gethostname()
-    local_ip = socket.gethostbyname(hostname)
-    print("""\u001b[36;1m
-  _____ _____ _____        __      
- |_   _|  __ \_   _|      / _|     
-   | | | |__) || |  _ __ | |_ ___  
-   | | |  ___/ | | | '_ \|  _/ _ \ 
-  _| |_| |    _| |_| | | | || (_) |
- |_____|_|   |_____|_| |_|_| \___/ 
-                                   
-                                   
-    \u001b[33m""")
-    print("Your computer name is: " + hostname)
-    print('Public IPv4 address is: {}'.format(publicip))
-    print("Local IPv4 address is: " + local_ip)
-    input("Press enter to contiune.")
+# Network information applet
+def IPInfo():
+    netInfo = []
+
+    # Returns a list of network names and IP addresses
+    # List indexes:
+    # 0 = hostname
+    # 1 = private IP address
+    # 2 = public IP address
+    def GetIP():
+        # Get the hostname of the computer
+        hostname = socket.gethostname()
+        # Get the private IP address starting with "192.168."
+        privateIP = [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if ip.startswith("192.168.")][:1]
+        # Get the public IP address
+        publicIP = get("https://api.ipify.org").text
+        netInfo.insert(0, hostname)
+        netInfo.insert(1, privateIP[0])
+        netInfo.insert(2, publicIP)
+
+    ClearScreen()
+    GetIP()
+    #publicIP = get("https://api.ipify.org").text
+    #hostname = socket.gethostname()
+    #local_ip = socket.gethostbyname(hostname)
+    #localIP = [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if ip.startswith("192.")][:1]
+    #local_ip = GetIP()
+    title = open(os.path.join(titleDir, "title-ipinfo.txt"))
+    PrintMessage(title.read(), "none", BrightCyan)
+    PrintMessage("Your computer name is: " + netInfo[0])
+    PrintMessage("Private IP address is: " + netInfo[1])
+    PrintMessage("Public IP address is: " + netInfo[2])
+    input("Press ENTER to continue.")
     
+# Fixes a weird bug in Windows command line to allow printing of colors. 
+ClearScreen()
+PrintMessage("hello World", "customprefix", BrightBlue)
 
-
-
-def maindashboard():
-    if os.name =='posix':
-        os.system('clear')
-    else:
-       os.system('cls')
-    print("""\u001b[36;1m
-      _____           _           _      _____                         _         
-     |  __ \         (_)         | |    |  __ \                       | |        
-     | |__) | __ ___  _  ___  ___| |_   | |__) |___   __ _ _ __   ___ | | _____  
-     |  ___/ '__/ _ \| |/ _ \/ __| __|  |  _  // _ \ / _` | '_ \ / _ \| |/ / _ \ 
-     | |   | | | (_) | |  __/ (__| |_   | | \ \ (_) | (_| | | | | (_) |   <  __/ 
-     |_|   |_|  \___/| |\___|\___|\__|  |_|  \_\___/ \__,_|_| |_|\___/|_|\_\___| 
-                    _/ |                                                         
-                   |__/                                                                        
-    """)
-
-
-
-
-
-
-
-    print("""
-    \u001b[33mWireless Atacks           Password Attacks          Database Attacks\u001b[32m
-    (1)Airgeddon              (4)                       (7)
-    (2)Fern Wifi Cracker      (5)                       (8)
-    (3)                       (6)                       (9)
-    \u001b[33mNetworking Tools          OSINT Tools               Exploitation Tools\u001b[32m
-    (10)Ip Info Applet        (13)OSINT Framework       (16)
-    (11)                      (14)                      (17)
-    (12)                      (15)                      (18)
-    """)
-    userinput()
 while(1):
-    maindashboard()
-    
+    MainDashboard()
